1d0f88bf26922258e3ad59dae0a6369ee5b0f4b5
SeanPhilippi <sean.philippi@pm.me>
2023-08-30 14:32:33 -0400

    material ui added and submit button started, rendering

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,27 +1,38 @@
 import React from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import Button from '@mui/material/Button';
 
 const Map: React.FC = () => {
   // latitude, longitude
   const position: [number, number] = [51.505, -0.09];
 
   return (
-    <MapContainer
-      style={{ height: '80vh', width: '100vh' }}
-      center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
-    >
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      />
-      <Marker position={position}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
-    </MapContainer>
+    <>
+      <MapContainer
+        style={{ height: '80vh', width: '100vh' }}
+        center={[51.505, -0.09]}
+        zoom={13}
+        scrollWheelZoom={false}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      <Button
+        style={{ marginTop: '1rem' }}
+        variant='contained'
+        color='primary'
+        onClick={() => console.log('clicked')}
+      >
+        New Submission
+      </Button>
+    </>
   );
 };
 
