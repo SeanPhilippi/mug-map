da39bb12df82854f5e7e0ef37e17f9283c5b6c56
SeanPhilippi <sean.philippi@pm.me>
2023-09-01 17:23:59 -0400

    scroll wheel now zooms

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -31,7 +31,7 @@ const Map: React.FC = () => {
         style={{ height: '80vh', width: '95vw' }}
         center={[51.505, -0.09]}
         zoom={13}
-        scrollWheelZoom={false}
+        scrollWheelZoom={true}
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
