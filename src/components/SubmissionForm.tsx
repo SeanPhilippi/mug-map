807955fd75272eac608d23b45b0b8b20c3813e69
SeanPhilippi <sean.philippi@gmail.com>
2023-09-11 21:32:05 -0400

    fetch => axios, data ready for api

--- src/components/SubmissionForm.tsx
+++ src/components/SubmissionForm.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { TextField, Checkbox, FormControlLabel, Button, Typography } from '@material-ui/core';
 import { useFetch } from '../hooks/useFetch.ts';
@@ -21,45 +21,105 @@ const SubmissionForm = () => {
   console.log('data', data);
   console.log('error', error);
 
+  const [formFields, setFormFields] = useState({
+    // text fields
+    name: '',
+    address1: '',
+    address2: '',
+    city: '',
+    country: '',
+    zip: '',
+    email: '',
+    instagram: '',
+    facebook: '',
+    x: '',
+    website: '',
+    // checkboxes
+    hasMugs: false,
+    no_mugs: false,
+    wifi: false,
+    work_friendly: false,
+    // text fields
+    description: '',
+    submitter_name: '',
+    submitter_email: '',
+    message_to_admin: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormFields({
+      ...formFields,
+      [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    query('test', formFields);
+  }
+
   return (
     <form
       className={classes.form}
-      onSubmit={e => {
-        e.preventDefault();
-        query('test');
-      }}
+      onSubmit={handleSubmit}
     >
       <Typography variant='h6'>Business Information</Typography>
       <TextField
+        name='name'
         label='Business Name*'
         required
+        onChange={handleChange}
       />
-      {/* <TextField
+      <TextField
+        name='address1'
         label='Address (Street 1)*'
         required
+        onChange={handleChange}
       />
       <TextField label='Address (Street 2)' />
       <TextField
+        name='address2'
         label='City*'
         required
+        onChange={handleChange}
       />
       <TextField
+        name='country'
         label='Country*'
         required
+        onChange={handleChange}
       />
       <TextField
-        label='ZIP*'
+        name='zip'
+        label='Zip*'
         required
+        onChange={handleChange}
       />
-      <TextField label='Phone' />
       <TextField
+        name='phone'
+        label='Phone'
+      />
+      <TextField
+        name='email'
         label='Email'
         type='email'
       />
-      <TextField label='Instagram' />
-      <TextField label='Facebook' />
-      <TextField label='X' />
-      <TextField label='Website' />
+      <TextField
+        name='instagram'
+        label='Instagram'
+      />
+      <TextField
+        name='facebook'
+        label='Facebook'
+      />
+      <TextField
+        name='x'
+        label='X'
+      />
+      <TextField
+        name='Website'
+        label='Website'
+      />
       <Typography
         variant='h6'
         component='div'
@@ -68,40 +128,50 @@ const SubmissionForm = () => {
       </Typography>
       <FormControlLabel
         control={<Checkbox />}
+        name='has_mugs'
         label='Has Mugs'
       />
       <FormControlLabel
         control={<Checkbox />}
+        name='no_mugs'
         label='No Mugs'
       />
       <FormControlLabel
         control={<Checkbox />}
+        name='wifi'
         label='WiFi'
       />
       <FormControlLabel
         control={<Checkbox />}
+        name='work_friendly'
         label='Work-friendly'
       />
       <TextField
+        name='description'
         label='Description'
         multiline
         rows={4}
       />
       <Typography variant='h6'>Submitter Information</Typography>
       <TextField
+        name='submitter_name'
         label='Submitter Name*'
         required
+        onChange={handleChange}
       />
       <TextField
+        name='submitter_email'
         label='Submitter Email*'
         type='email'
         required
+        onChange={handleChange}
       />
       <TextField
+        name='message_to_admin'
         label='Message to Admin'
         multiline
-        rows={4}
-      /> */}
+        minRows={4}
+      />
       <Button
         type='submit'
         variant='contained'
