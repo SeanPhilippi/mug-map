0a261edace906762c95236e5c0aae71b9df82b8b
SeanPhilippi <sean.philippi@gmail.com>
2023-11-14 13:31:53 -0500

    UpdateForm started and css changes and update listing button added to Details

--- src/components/screens/Details/Details.tsx
+++ src/components/screens/Details/Details.tsx
@@ -3,13 +3,22 @@ import { useParams, useHistory } from 'react-router-dom';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import {
   Twitter as TwitterIcon,
   Facebook as FacebookIcon,
   Instagram as InstagramIcon,
   Language as WebsiteIcon,
 } from '@material-ui/icons';
+import Dialog from '@material-ui/core/Dialog';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import IconButton from '@material-ui/core/IconButton';
+import CloseIcon from '@material-ui/icons/Close';
 import { makeStyles } from '@material-ui/core/styles';
+import UpdateForm from './UpdateForm';
 import { useFetch } from '../../../hooks/useFetch.js';
 import type { BusinessData } from '../../../types.js';
 
@@ -18,8 +27,14 @@ interface DetailsParams {
 }
 
 const useStyles = makeStyles({
-  detailsPage: {
-  }
+  detailsPage: {},
+  footerButtons: {
+    margin: '5rem auto',
+  },
+  button: {
+    // flex: 1,
+    width: '10rem',
+  },
 });
 
 const Details: FC = () => {
@@ -42,6 +57,16 @@ const Details: FC = () => {
     fetchBusinessData();
   }, []);
 
+  const [open, setOpen] = useState(false);
+
+  const handleOpenUpdateForm = () => {
+    setOpen(true);
+  };
+
+  const handleCloseUpdateForm = () => {
+    setOpen(false);
+  };
+
   const handleGoBack = () => {
     history.goBack(); // Go back to the previous screen
   };
@@ -52,79 +77,131 @@ const Details: FC = () => {
 
   return (
     business && (
-      <div className={classes.detailsPage}>
-        <Typography variant='h4'>Business Details</Typography>
+      <Card>
+        <CardContent>
+          <Typography variant='h4'>Business Details</Typography>
 
-        <Typography variant='h6'>Location:</Typography>
-        <Typography>{business.name}</Typography>
-        <Typography>{business.address1}</Typography>
-        <Typography>{business.address2}</Typography>
-        <Typography>{business.city}</Typography>
-        <Typography>{business.state}</Typography>
-        <Typography>{business.country}</Typography>
-        <Typography>{business.zip}</Typography>
+          <Typography variant='h6'>Location:</Typography>
+          <Typography>{business.name}</Typography>
+          <Typography>{business.address1}</Typography>
+          <Typography>{business.address2}</Typography>
+          <Typography>{business.city}</Typography>
+          <Typography>{business.state}</Typography>
+          <Typography>{business.country}</Typography>
+          <Typography>{business.zip}</Typography>
 
-        <Typography variant='h6'>Contact:</Typography>
-        <Typography>{business.phone}</Typography>
-        <Typography>{business.email}</Typography>
-        <Typography>{business.instagram}</Typography>
-        <Typography>{business.facebook}</Typography>
+          <Typography variant='h6'>Contact:</Typography>
+          <Typography>{business.phone}</Typography>
+          <Typography>{business.email}</Typography>
+          <Typography>{business.instagram}</Typography>
+          <Typography>{business.facebook}</Typography>
 
-        <Typography variant='h6'>Social Media:</Typography>
-        {business.website && (
-          <Link
-            href={business.website}
-            target='_blank'
-            rel='noreferrer'
+          <Typography variant='h6'>Social Media:</Typography>
+          {business.website && (
+            <Link
+              href={business.website}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <WebsiteIcon /> {business.website}
+            </Link>
+          )}
+          {business.x && (
+            <Link
+              href={`https://x.com/${business.x}`}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <TwitterIcon /> {business.x}
+            </Link>
+          )}
+          {business.facebook && (
+            <Link
+              href={business.facebook}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <FacebookIcon /> {business.facebook}
+            </Link>
+          )}
+          {business.instagram && (
+            <Link
+              href={`https://instagram.com/${business.instagram}`}
+              target='_blank'
+              rel='noreferrer'
+            >
+              <InstagramIcon /> {business.instagram}
+            </Link>
+          )}
+
+          <Typography variant='h6'>Features:</Typography>
+          <Typography>
+            Offers Mugs: {business.offers_mugs === null ? 'N/A' : business.offers_mugs ? 'Yes' : 'No'}
+          </Typography>
+          <Typography>
+            Accepts Personal Mug:{' '}
+            {business.accepts_personal_mug === null ? 'N/A' : business.accepts_personal_mug ? 'Yes' : 'No'}
+          </Typography>
+          <Typography>Wifi: {business.wifi === null ? 'N/A' : business.wifi ? 'Yes' : 'No'}</Typography>
+          <Typography>
+            Work Friendly: {business.work_friendly === null ? 'N/A' : business.work_friendly ? 'Yes' : 'No'}
+          </Typography>
+          <Typography>
+            Sufficient Outlets:{' '}
+            {business.sufficient_outlets === null ? 'N/A' : business.sufficient_outlets ? 'Yes' : 'No'}
+          </Typography>
+
+          <Typography variant='h6'>Description:</Typography>
+          <Typography>{business?.description}</Typography>
+
+          <Grid
+            container
+            spacing={3}
+            justifyContent='center'
+            className={classes.footerButtons}
           >
-            <WebsiteIcon /> {business.website}
-          </Link>
-        )}
-        {business.x && (
-          <Link
-            href={`https://x.com/${business.x}`}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <TwitterIcon /> {business.x}
-          </Link>
-        )}
-        {business.facebook && (
-          <Link
-            href={business.facebook}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <FacebookIcon /> {business.facebook}
-          </Link>
-        )}
-        {business.instagram && (
-          <Link
-            href={`https://instagram.com/${business.instagram}`}
-            target='_blank'
-            rel='noreferrer'
-          >
-            <InstagramIcon /> {business.instagram}
-          </Link>
-        )}
-
-        <Typography variant='h6'>Features:</Typography>
-        <Typography>Offers Mugs: {business.offers_mugs === null ? 'N/A' : (business.offers_mugs ? 'Yes' : 'No')}</Typography>
-        <Typography>Accepts Personal Mug: {business.accepts_personal_mug === null ? 'N/A' : (business.accepts_personal_mug ? 'Yes' : 'No')}</Typography>
-        <Typography>Wifi: {business.wifi === null ? 'N/A' : (business.wifi ? 'Yes' : 'No')}</Typography>
-        <Typography>Work Friendly: {business.work_friendly === null ? 'N/A' : (business.work_friendly ? 'Yes' : 'No')}</Typography>
-        <Typography>Sufficient Outlets: {business.sufficient_outlets === null ? 'N/A' : (business.sufficient_outlets ? 'Yes' : 'No')}</Typography>
-
-        <Typography variant='h6'>Description:</Typography>
-        <Typography>{business?.description}</Typography>
-
-        <Button
-          variant='contained'
-          onClick={handleGoBack}
-        >
-          Go Back
-        </Button>
-      </div>
+            <Grid item>
+              <Button
+                variant='contained'
+                onClick={handleGoBack}
+                className={classes.button}
+              >
+                Go Back
+              </Button>
+            </Grid>
+            <Grid item>
+              <Button
+                variant='contained'
+                onClick={handleOpenUpdateForm}
+                className={classes.button}
+              >
+                Update Listing
+              </Button>
+              <Dialog
+                open={open}
+                onClose={handleCloseUpdateForm}
+                aria-labelledby='form-dialog-title'
+              >
+                <DialogTitle id='form-dialog-title'>
+                  <div style={{ width: '100%', display: 'flex', justifyContent: 'space-between' }}>
+                    Submission Form
+                    <IconButton
+                      color='inherit'
+                      onClick={handleCloseUpdateForm}
+                      aria-label='close'
+                    >
+                      <CloseIcon />
+                    </IconButton>
+                  </div>
+                </DialogTitle>
+                <DialogContent>
+                  <UpdateForm handleClose={handleCloseUpdateForm} />
+                </DialogContent>
+              </Dialog>
+            </Grid>
+          </Grid>
+        </CardContent>
+      </Card>
     )
   );
 };
---
+++ src/components/screens/Details/UpdateForm.tsx
@@ -0,0 +1,331 @@
+import React, { useState, FC } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import { useFetch } from '../../../hooks/useFetch.js';
+import axios, { AxiosResponse } from 'axios';
+
+const useStyles = makeStyles(theme => ({
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    // ! look at these styles, don't seem to be working
+    gap: theme.spacing(2),
+  },
+  updateButton: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+const getCoordinatesFromOpenCage = async (address: string) => {
+  const apiKey = import.meta.env.VITE_APP_OPEN_CAGE_KEY;
+  const response: AxiosResponse = await axios.get(
+    `https://api.opencagedata.com/geocode/v1/json?q=${encodeURIComponent(address)}&key=${apiKey}`,
+  );
+  return response.data.results[0].geometry;
+};
+
+interface UpdateFormProps {
+  handleClose: () => void;
+}
+
+// ! ADD VALIDATION TO FORM FIELDS THAT NEED IT
+// ! phone
+const UpdateForm: FC<UpdateFormProps> = ({ handleClose }) => {
+  const classes = useStyles();
+
+  const { data, error, query } = useFetch();
+  console.log('data', data);
+  console.log('error', error);
+
+  const [formFields, setFormFields] = useState({
+    // text fields
+    name: '',
+    address1: '',
+    address2: '',
+    city: '',
+    state: '',
+    country: '',
+    zip: '',
+    phone: '',
+    email: '',
+    instagram: '',
+    facebook: '',
+    x: '',
+    website: '',
+    // checkboxes
+    offers_mugs: 'notSure',
+    accepts_personal_mug: 'notSure',
+    wifi: 'notSure',
+    work_friendly: 'notSure',
+    sufficient_outlets: 'notSure',
+    // text fields
+    description: '',
+    submitter_name: '',
+    submitter_email: '',
+    message_to_admin: '',
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log('==name', e.target.name);
+    console.log('==checked', e.target.checked);
+    console.log('==value', e.target.value);
+    setFormFields({
+      ...formFields,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const address = `${formFields.address1} ${formFields.address2}, ${formFields.city}, ${formFields.state}, ${formFields.country}`;
+    const coords = await getCoordinatesFromOpenCage(address);
+    console.log('==formFields', formFields);
+    console.log('==coords', coords);
+    const additionalInfoMap = {
+      notSure: null,
+      no: false,
+      yes: true,
+    };
+    const additionalInfo = {
+      offers_mugs: additionalInfoMap[formFields.offers_mugs],
+      accepts_personal_mug: additionalInfoMap[formFields.accepts_personal_mug],
+      wifi: additionalInfoMap[formFields.wifi],
+      work_friendly: additionalInfoMap[formFields.work_friendly],
+      sufficient_outlets: additionalInfoMap[formFields.sufficient_outlets],
+    };
+    console.log('==additionalInfo', additionalInfo);
+    const data = await query('businesses', { ...formFields, ...coords, ...additionalInfo });
+    console.log('==data', data);
+    handleClose();
+  };
+
+  return (
+    <form
+      className={classes.form}
+      onSubmit={handleSubmit}
+      style={{ width: '33rem' }}
+    >
+      <Typography variant='h6'>Business Information</Typography>
+      <TextField
+        name='name'
+        label='Business Name'
+        variant='outlined'
+        required
+        onChange={handleChange}
+      />
+      <TextField
+        name='address1'
+        label='Address (Street 1)'
+        variant='outlined'
+        required
+        onChange={handleChange}
+      />
+      <TextField
+        name='address2'
+        label='Address (Street 2)'
+        variant='outlined'
+        onChange={handleChange}
+      />
+      <TextField
+        name='city'
+        label='City'
+        required
+        variant='outlined'
+        onChange={handleChange}
+      />
+      <TextField
+        name='state'
+        label='State/Province/Region'
+        required
+        variant='outlined'
+        onChange={handleChange}
+      />
+      {/* consider making this a select element */}
+      <TextField
+        name='country'
+        label='Country'
+        variant='outlined'
+        required
+        onChange={handleChange}
+      />
+      <TextField
+        name='zip'
+        label='Zip'
+        variant='outlined'
+        required
+        type='number'
+        onChange={handleChange}
+      />
+      {/* ! add auto-formatting to d-ddd-ddd-dddd format and only allow number input */}
+      <TextField
+        name='phone'
+        label='Phone'
+        variant='outlined'
+        type='number'
+        onChange={handleChange}
+      />
+      <TextField
+        name='email'
+        label='Email'
+        variant='outlined'
+        type='email'
+        onChange={handleChange}
+      />
+      <Typography
+        variant='h6'
+        component='div'
+      >
+        Social Media
+      </Typography>
+      <TextField
+        name='instagram'
+        label='Instagram'
+        variant='outlined'
+        placeholder='@'
+        onChange={handleChange}
+      />
+      <TextField
+        name='facebook'
+        label='Facebook'
+        variant='outlined'
+        placeholder='facebook.com/'
+        onChange={handleChange}
+      />
+      <TextField
+        name='x'
+        label='X'
+        variant='outlined'
+        placeholder='@'
+        onChange={handleChange}
+      />
+      {/* <TextField
+        name='youtube'
+        label='Youtube'
+        variant='outlined'
+        placeholder='youtube.com/
+        onChange={handleChange}
+      /> */}
+      <TextField
+        name='website'
+        label='Website'
+        variant='outlined'
+        onChange={handleChange}
+      />
+      <Typography
+        variant='h6'
+        component='div'
+      >
+        Additional Information
+      </Typography>
+      <FormControl>
+        <InputLabel>Offers Mugs?</InputLabel>
+        <Select
+          value={formFields.offers_mugs}
+          onChange={handleChange}
+          name='offers_mugs'
+        >
+          <MenuItem value='notSure'>Not sure</MenuItem>
+          <MenuItem value='yes'>Yes</MenuItem>
+          <MenuItem value='no'>No</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Accepts Personal Mug?</InputLabel>
+        <Select
+          value={formFields.accepts_personal_mug}
+          onChange={handleChange}
+          name='accepts_personal_mug'
+        >
+          <MenuItem value='notSure'>Not sure</MenuItem>
+          <MenuItem value='yes'>Yes</MenuItem>
+          <MenuItem value='no'>No</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Wifi?</InputLabel>
+        <Select
+          value={formFields.wifi}
+          onChange={handleChange}
+          name='wifi'
+        >
+          <MenuItem value='notSure'>Not sure</MenuItem>
+          <MenuItem value='yes'>Yes</MenuItem>
+          <MenuItem value='no'>No</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Work-friendly?</InputLabel>
+        <Select
+          value={formFields.work_friendly}
+          onChange={handleChange}
+          name='work_friendly'
+        >
+          <MenuItem value='notSure'>Not sure</MenuItem>
+          <MenuItem value='yes'>Yes</MenuItem>
+          <MenuItem value='no'>No</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl>
+        <InputLabel>Sufficient Outlets?</InputLabel>
+        <Select
+          value={formFields.sufficient_outlets}
+          onChange={handleChange}
+          name='sufficient_outlets'
+        >
+          <MenuItem value='notSure'>Not sure</MenuItem>
+          <MenuItem value='yes'>Yes</MenuItem>
+          <MenuItem value='no'>No</MenuItem>
+        </Select>
+      </FormControl>
+      <TextField
+        name='description'
+        label='Description'
+        variant='outlined'
+        multiline
+        minRows={4}
+        onChange={handleChange}
+      />
+      <Typography variant='h6'>Submitter Information</Typography>
+      <TextField
+        name='submitter_name'
+        label='Submitter Name'
+        variant='outlined'
+        required
+        onChange={handleChange}
+      />
+      <TextField
+        name='submitter_email'
+        label='Submitter Email'
+        variant='outlined'
+        type='email'
+        required
+        onChange={handleChange}
+      />
+      <TextField
+        name='message_to_admin'
+        label='Message to Admin'
+        variant='outlined'
+        multiline
+        minRows={4}
+        onChange={handleChange}
+      />
+      <Button
+        type='submit'
+        variant='contained'
+        color='primary'
+        className={classes.updateButton}
+      >
+        Update
+      </Button>
+    </form>
+  );
+};
+
+export default UpdateForm;
+
